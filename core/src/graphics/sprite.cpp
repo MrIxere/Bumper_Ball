16da9e98a777cd2e892149225d98829a31b5c250
SandyWoolman <71042617+SandyWoolman@users.noreply.github.com>
2021-10-18 14:16:31 +0200

    sprite has correct size for now

--- core/src/graphics/sprite.cpp
+++ core/src/graphics/sprite.cpp
@@ -29,7 +29,7 @@ namespace core
                 if(entityManager_.HasComponent(entity, static_cast<Component>(ComponentType::SCALE)))
                 {
                     const auto scale = transformManager_.GetScale(entity);
-                    components_[entity].setScale(scale.x, scale.y);
+                    components_[entity].setScale(scale.x *2, scale.y *2);
                 }
                 if (entityManager_.HasComponent(entity, static_cast<Component>(ComponentType::ROTATION)))
                 {
